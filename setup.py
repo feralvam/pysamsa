f3edc657ba45fb4fa3a3dfe6fe888009d480956b
Louis MARTIN <louisrtm@gmail.com>
2018-09-20 06:49:14 -0700

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='pysamsa',
+      version='1.0',
+      description='SAMSA Metric for Text Simplification',
+      author='Louis Martin',
+      author_email='louisrtm@gmail.com',
+      packages=['pysamsa'])
